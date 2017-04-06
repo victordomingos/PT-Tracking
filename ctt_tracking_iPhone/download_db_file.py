3313f870187b4747a188faa162d9fa09ecab02bf
Victor Domingos <victordomingos@users.noreply.github.com>
2017-04-06 22:12:17 +0100

    Create download_db_file.py

---
+++ ctt_tracking_iPhone/download_db_file.py
@@ -0,0 +1,21 @@
+import requests
+import console
+import time
+import os
+import runpy
+import time
+from zipfile import ZipFile
+import io
+import gzip
+
+os.chdir('/private/var/mobile/Containers/Shared/AppGroup/5E22383E-122A-4BBE-9677-E4A0EF63E7C3/Pythonista3/Documents/ctt_tracking_ios')
+
+# personalizar a linha seguinte com o URL da mini_db no Dropbox, por exemplo:
+ZIP_MINI_DB = 'mini_db_PT_Tracking.sqlite.zip?raw=1'
+
+#print("\nA obter base de dados atualizada...")
+console.show_activity()
+pedido = requests.get(ZIP_MINI_DB)
+ficheiro = ZipFile(io.BytesIO(pedido.content))
+console.hide_activity()
+ficheiro.extract('mini_db_PT_Tracking.sqlite')

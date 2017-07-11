3f7c96e371d2cfb4e314931f1fdca55e9c555d54
Victor Domingos <victordomingos@users.noreply.github.com>
2017-07-11 19:47:30 +0100

    Update ctt_tracking.py

--- ctt_tracking/ctt_tracking.py
+++ ctt_tracking/ctt_tracking.py
@@ -39,7 +39,7 @@ from logging_stuff import *
 __author__ = "Victor Domingos"
 __copyright__ = "Copyright 2017 Victor Domingos"
 __license__ = "Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)"
-__version__ = "v.2.11"
+__version__ = "v.2.12"
 __email__ = "web@victordomingos.com"
 __status__ = "beta"
 
